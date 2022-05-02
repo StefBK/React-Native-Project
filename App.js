6a1d95a09e4cb2dbd0ce5e2538b29bb8752f1b7d
Stef BK <stefsbk@gmail.com>
2022-05-02 13:10:08 +0200

    Initial Commit

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import dummy from './assets/dummy-image-square.jpg';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const openImagePicker=async()=>{
+    const permission=await ImagePicker.requestMediaLibraryPermissionsAsync();
+    console.log(permission);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={dummy} style={{width:300, height:300}}/>
+      {/* <Image source={{uri:'https://www.drome.cci.fr/sites/g/files/mwbcuj971/files/2020-02/LOGO-CCI--BASE-LINE-WEB-2019_0.jpg'}} style={{width:300, height:300}}/> */}
+      {/* <Image source={require ('./assets/dummy-image-square.jpg')} style={{width:300, height:300}}/>
+      <Text style={{color: 'darkblue', fontsize: 18}}>To share a photo, please press the button bellow!</Text> */}
+      <TouchableOpacity
+        onPress={openImagePicker}
+        style={styles.button}>
+      <Text style={styles.textWhite}>Pick a photo</Text>  
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +31,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button:{
+    backgroundColor: 'blue',
+    padding: 10
+  },
+  textWhite:{
+    color:'#FFFF',
+    margin:0
+  }
 });
