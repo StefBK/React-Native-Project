75524ea3df5dac2d49115416d6c78891a5464238
Stef BK <stefsbk@gmail.com>
2022-05-02 15:17:09 +0200

    Sharing Options

--- App.js
+++ App.js
@@ -1,13 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Platform } from 'react-native';
 import dummy from './assets/dummy-image-square.jpg';
 import * as ImagePicker from 'expo-image-picker';
+import * as Sharing from 'expo-sharing';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [image,setImage]=useState(null);
+
+  const openSharing=async()=>{
+  const canIShare=await Sharing.isAvailableAsync();
+  console.log('canIShare : ',canIShare);
+  console.log(Platform);
+    if (Platform.OS!=='web'){
+      await Sharing.shareAsync(image);
+  } else{
+      alert('You cannot share on your platform!')
+    }
+  }
+  
   const openImagePicker=async()=>{
     const permission=await ImagePicker.requestMediaLibraryPermissionsAsync();
     console.log(permission);
+    if(permission.granted===false){
+      alert('Permission to access your library is required')
+      return;
+    }
+    const pickerResult=await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+    if(pickerResult.cancelled){
+      return;
+    }
+    setImage(pickerResult.uri);
   }
+
+  if(image!==null){
+    return(
+      <View style={styles.container}>
+      <Image source={{uri:image}} style={{width:300, height:300}}/>
+      <TouchableOpacity
+        onPress={openSharing}
+        style={styles.button}>
+      <Text style={styles.textWhite}>Share your photo</Text>  
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={()=>{setImage(null)}}
+        style={styles.button}>
+      <Text style={styles.textWhite}>Cancel</Text>  
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Image source={dummy} style={{width:300, height:300}}/>
